d352697e3513c33a0eb14a9a7f94a11e7911f23d
Ryan Mukai <rmukai68@gmail.com>
2019-07-26 04:38:31 +0000

    Working on Jupyter port

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
         name='logic_processor',
         version='0.1dev',
         packages=['nn_utils','neural'],
-	install_requires=['numpy','matplotlib','tensorflow','keras'],
+        install_requires=['numpy','matplotlib',
+        'tensorflow','keras','pandas','jupyter'],
         author='Ryan Mukai',
         license='MIT'
                             )
