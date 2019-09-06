c2b1ea6779b87e6e0c1653e3d89a69a236f684a8
Ryan Mukai <rmukai68@gmail.com>
2019-09-06 14:45:00 +0000

    Changes to accomodate new neural network

--- nn_utils/config.py
+++ nn_utils/config.py
@@ -15,7 +15,9 @@ logging.basicConfig(filename=CONFIG_LOG_FILE, level=logging.INFO)
 
 CONFIG_SYMBOL_LIST = [
     '(', ')', 'bookend', 'and', 'or', 'not', 'forall',
+    'xor',
     '=', 'is', 'true', 'false', 'unknown', 'exists',
     'believes', 'knows', 'name', 'me', 'what', '.', '?',
     'if', 'then', 'help', "recite", None
 ]
+
